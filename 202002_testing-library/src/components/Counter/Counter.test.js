45d664d7b0315cb468a6615419733171571644c4
milkmidi <milkmidi@medialand.tw>
2020-03-09 16:31:45 +0800

    updat Counter.test.js

--- 202002_testing-library/src/components/Counter/Counter.test.js
+++ 202002_testing-library/src/components/Counter/Counter.test.js
@@ -12,26 +12,33 @@ describe('Counter', () => {
   afterEach(cleanup);
 
   test('increments value on click', async () => {
+    // render vue Component
     const { getByTestId } = render(Counter);
 
+    // 比對 data-testid="count" DOM 素的值是否為 0
     expect(getByTestId('count').textContent.trim()).toBe('0');
 
+    // 抓到 button 元素
     const button = getByTestId('button');
 
-    // Dispatch a native click event to our button element.
+    // 點擊二次
     await fireEvent.click(button);
     await fireEvent.click(button);
 
+    // 比對 data-testid="count" DOM 素的值是否為 2
     expect(getByTestId('count').textContent.trim()).toBe('2');
   });
 
   test('defaultCount', async () => {
+    // render vue Component, 並帶入 props 值
     const { getByTestId } = render(Counter, {
       propsData: {
         defaultCount: 10,
       },
     });
 
+    // testing-library 有提供一種非同步的比對方法
+    // 直接使用 wait 函式，可以參考以上的原始碼。
     // https://github.com/TheBrainFamily/wait-for-expect/blob/master/src/index.ts
     await wait(() => {
       expect(getByTestId('count').textContent.trim()).toBe('10');
