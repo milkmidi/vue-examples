f34579e32e63bf1a2de543b152af6447142cf869
milkmidi <milkmidi@medialand.tw>
2020-05-04 11:50:25 +0800

    add 202005_your-component

---
+++ 202005_your-component/src/componentEntry.js
@@ -0,0 +1,21 @@
+﻿
+import Vue from 'vue';
+import MyNavigation from '@/components/MyNavigation.vue';
+import MyFooter from '@/components/MyFooter.vue';
+
+if (typeof window.Vue === 'undefined') {
+  throw new Error('Vue is not available in your browser.');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const myComponents = document.querySelectorAll('my-navigation,my-footer');
+  [].forEach.call(myComponents, (el:HTMLElement) => {
+    new Vue({
+      el,
+      components: {
+        'my-navigation': MyNavigation,
+        'my-footer': MyFooter,
+      },
+    }).$mount(el);
+  });
+});
---
+++ 202005_your-component/src/components/MyNavigation.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="my-navigation">
+    <a href="http://www.google.com">webpack,vue,選單選單1</a>
+    <a href="http://www.google.com">webpack,vue,選單選單2</a>
+  </div>
+</template>
+
+<style scoped>
+.my-navigation{
+  background:black;
+}
+a{
+  color:red;
+}
+</style>
